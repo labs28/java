357dc9de408af656cc8342222d97635e59d24b6b
labs28 <darksoulimmortal@gmail.com>
2025-12-14 21:42:30 +0530

    Implement thread synchronization example with Table class

---
+++ threadsync.java
@@ -0,0 +1,42 @@
+class Table {
+    // thread synchronization
+    synchronized void printTable(int n) {
+        for(int i = 1; i <= 10; i++) {
+            System.out.println(n * i);
+        }
+    }
+}
+
+class Thread1 extends Thread {
+    Table t;
+    Thread1(Table t)
+    {
+        this.t = t;
+    }
+    public void run() {
+        t.printTable(2);
+    }
+}
+
+class Thread2 extends Thread {
+    Table t;
+    Thread2(Table t)
+    {
+        this.t = t;
+    }
+    public void run() {
+        t.printTable(3);
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Table obj = new Table();
+
+        Thread1 t1 = new Thread1(obj);
+        Thread2 t2 = new Thread2(obj);
+
+        t1.start();
+        t2.start();
+    }
+}
